766dc86e5a537ec3a20cf3e3cb118dd75808a951
MajdAlkhalaf12 <majd03.hussain@gmail.com>
2024-07-24 22:23:22 +0300

    important text box

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import anas from "../public/imges/anas.svg";
 import majd from "../public/imges/majd.svg";
 import ProgressBar from "./components/ProgressBar";
 import Drawer from "./components/ExamSheetDrawer";
+import ImportantTextBox from "./components/styles/ImportantTextBox";
 function App() {
   return (
     <div>
-      <div className="gap-10">
+      <div>
         <ProgressBar value="8" max="9" />
 
         <div className="flex flex-row items-end">
@@ -18,7 +19,7 @@ function App() {
         </div>
 
         <div className="flex flex-row items-end">
-          <NormalTextBox>مونتي كارلو لك عمي شو مفكر</NormalTextBox>
+          <ImportantTextBox>مونتي كارلو لك عمي شو مفكر</ImportantTextBox>
           <img src={majd} alt="anas" className="max-w-20" />
         </div>
 
---
+++ src/components/styles/ImportantTextBox.tsx
@@ -0,0 +1,31 @@
+import { TextGenerateEffect } from "./TextGenerateEffect";
+
+const detectScript = (text: string) => {
+  const arabicPattern = /[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF]/;
+  return arabicPattern.test(text) ? "arabic" : "latin";
+};
+
+const ImportantTextBox = ({ children }: { children: string }) => {
+  const text = children?.toString() || "";
+  const script = detectScript(text);
+  const isArabic = script === "arabic";
+
+  return (
+    <div className="chat chat-end sm:max-w-80 w-120 ">
+      <div className="chat-bubble bg-transparent  border-gray-600 border-4 border-opacity-45 border-dashed">
+        <p
+          dir={isArabic ? "rtl" : "ltr"}
+          className={
+            isArabic
+              ? "font-swissra font-normal tracking-wide leading-loose"
+              : "font-medium font-sans tracking-wide leading-loose"
+          }
+        >
+          <TextGenerateEffect words={children} />
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ImportantTextBox;
